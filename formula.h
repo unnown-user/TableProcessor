c762203ed264376ba6f1cbaf11798121a127a981
unnown-user <alexsem2ev@gmail.com>
2026-06-03 13:36:26 +0700

    Update formula.h

--- formula.h
+++ formula.h
@@ -3,6 +3,7 @@
 
 #include "eval.h"
 
+
 typedef struct Formula Formula;
 
 typedef enum {
@@ -13,8 +14,31 @@ typedef enum {
     NODE_FUNC_SUM, NODE_FUNC_MIN, NODE_FUNC_MAX
 } NodeType;
 
+struct Formula {
+    NodeType type;
+    union {
+        double number;
+        struct {
+            int row;
+            int col;
+        } cell;
+        struct {
+            int r1, c1, r2, c2;
+        } range;
+        struct {
+            Formula* left;
+            Formula* right;
+        } binary;
+        struct {
+            Formula* child;
+        } unary;
+    };
+};
+
+
 Formula* formula_parse(const char* expr);
 void formula_free(Formula* f);
 double formula_evaluate(Formula* f, void* ctx);
+
 
 #endif
